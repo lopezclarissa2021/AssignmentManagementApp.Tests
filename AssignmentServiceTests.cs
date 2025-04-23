8a0473f8c10b9d3ec9f0e069a7792c8a6cdfdc4c
Clarissa Lopez <lopezclarissa2021@gmail.com>
2025-04-23 08:05:46 -0700

    deleted unused .cs file

---
+++ AssignmentServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using AssignmentManagementApp.Services;
+
+namespace AssignmentManagementApp.Tests
+{
+    public class AssignmentServiceTests
+    {
+        [Fact]
+        public void AddAssignment_IsNull_ThrowsError()
+        {
+            var service = new AssignmentService();
+            Assert.Throws<ArgumentNullException>(() => service.AddAssignment(null));
+        }
+        [Fact]
+        public void AddAssignment_ValidAssignment_AddSuccessfully()
+        {
+            // Arrange
+            var service = new AssignmentService();
+            var assignment = new Assignment("Test Assignment", "Test Description", DateTime.Now.AddDays(7));
+            // Act
+            service.AddAssignment(assignment);
+            // Assert
+            Assert.Equal(new List<Assignment> {assignment}, service.GetAllAssignments());
+        }
+    }
+}
